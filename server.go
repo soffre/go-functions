93f5846e9d54ed72040aff46326d60d32c077cb2
Amanuel Fentahun <6186fre@gmail.com>
2024-10-14 01:45:50 +0300

    Initial Commit

---
+++ server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello world",
+		})
+	})
+
+	r.GET("/name", func(c *gin.Context) {
+		name := c.Param("name")
+
+		c.JSON(200, gin.H{
+			"message": fmt.Sprintf("hello, %s!", name),
+		})
+	})
+
+	r.Run(fmt.Sprintf(":%s", port))
+}
